9ac3a442dbc79d9949504536e9aa6639c66f3e2f
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2021-03-23 18:27:30 +0000

    First updates

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,60 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+get_proverb_dataset <- function(){
 
-hello <- function() {
-  print("Hello, world!")
+  test <- xml2::read_html("https://nightvale.fandom.com/wiki/List_of_Proverbs")
+
+  t <- test %>%
+    rvest::html_nodes("tr") %>%
+    rvest::html_text()
+
+  meta <- t[-1] %>%
+    data.frame(number = ., stringsAsFactors = FALSE) %>%
+    # Extract every odd row
+    dplyr::slice(which(dplyr::row_number() %% 2 == 1)) %>%
+    # Remove live shows
+    dplyr::slice(1:which(.$number == "Show No.\n\nTitle\n\nRelease Date\n")-1) %>%
+    # Split into component parts
+    tidyr::separate(number,
+             c("number","title", "airdate"),
+             sep = '\n\n') %>%
+    # Remove quotes
+    dplyr::mutate(title = stringr::str_replace_all(title,'\"',""),
+           number = stringr::str_replace_all(number,'\"',""),
+           airdate = stringr::str_replace_all(airdate,'\"',"")) %>%
+    # Remove trailing newline character from airdate/
+    dplyr::mutate(airdate = stringr::str_replace(airdate,"\n",""))
+
+  proverb <- t[-1] %>%
+    data.frame(proverb = ., stringsAsFactors = FALSE) %>%
+    # Extract every even row
+    dplyr::slice(which(dplyr::row_number() %% 2 == 0)) %>%
+    # Remove proverbs from live shows
+    dplyr::slice(1:which(.$proverb == "1\n\n\"Condos\"\n\n25 September 2013\n")-1) %>%
+    # Generate two new variables describing the nchar and nwords
+    dplyr::mutate(proverb_len_char = nchar(proverb),
+           proverb_len_word = stringr::str_count(proverb, '\\w+')) %>%
+    # Adjust random newlines at start and end of proverbs
+    dplyr::mutate(proverb = stringr::str_replace(proverb,"\n\n$",""),
+           proverb = stringr::str_replace(proverb,"\n$",""),
+           proverb = stringr::str_replace(proverb,"^\n","")) %>%
+    # Bind to meta-data
+    cbind(meta,.)
+
+}
+
+#' Title
+#'
+#' @param id
+#'
+#' @return
+#' @export
+proverb <- function(id) {
+
+  n_row <- seq_along(1:nrow(proverb_data))
+
+  if (!hasArg(id)) {
+    id <- sample(n_row,1)
+  }
+
+  message(proverb$proverb[id])
+
 }
---
+++ README.md
@@ -0,0 +1,38 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# nightvale
+
+<!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+[![R-CMD-check](https://github.com/mcguinlu/nightvale/workflows/R-CMD-check/badge.svg)](https://github.com/mcguinlu/nightvale/actions)
+<!-- badges: end -->
+
+The goal of nightvale is to …
+
+## Installation
+
+You can install the released version of nightvale from
+[CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("nightvale")
+```
+
+And the development version from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("mcguinlu/nightvale")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(nightvale)
+## basic example code
+```
